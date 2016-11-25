62b59cee0039dc5d9a94a583abca7d6e5b61354d
do-thai-duong <do.thai.duong@yielddesign.co.jp>
2016-11-25 16:19:42 +0700

    initial project

---
+++ system/cms/logs/log-2016-11-25.php
@@ -0,0 +1,10 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2016-11-25 08:24:26 --> Severity: Notice  --> unserialize(): Error at offset 0 of 288 bytes D:\PT\workspace\project_microad\system\codeigniter\libraries\Session\drivers\Session_cookie.php 761
+ERROR - 2016-11-25 09:17:59 --> Severity: Notice  --> Undefined index: lang_code D:\PT\workspace\project_microad\system\cms\hooks\pick_language.php 46
+ERROR - 2016-11-25 09:19:09 --> Severity: Notice  --> Undefined variable: lang D:\PT\workspace\project_microad\system\cms\hooks\pick_language.php 61
+ERROR - 2016-11-25 09:19:09 --> Severity: Notice  --> Undefined variable: lang D:\PT\workspace\project_microad\system\cms\hooks\pick_language.php 61
+ERROR - 2016-11-25 09:19:44 --> Severity: Notice  --> Undefined variable: lang D:\PT\workspace\project_microad\system\cms\hooks\pick_language.php 61
+ERROR - 2016-11-25 09:19:44 --> Severity: Notice  --> Undefined variable: lang D:\PT\workspace\project_microad\system\cms\hooks\pick_language.php 61
+ERROR - 2016-11-25 09:20:14 --> Severity: Notice  --> Array to string conversion D:\PT\workspace\project_microad\system\cms\hooks\pick_language.php 63
+ERROR - 2016-11-25 09:20:14 --> Severity: Notice  --> Array to string conversion D:\PT\workspace\project_microad\system\cms\hooks\pick_language.php 63
